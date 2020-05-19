5914da80020cfcc8264665c1b8e81fc4bdd40d8a
hwia-edu@gmail.com <hwia-edu@gmail.com>
2020-05-19 20:39:24 +0900

    .

--- Pointer/001_VIrtualAddressSpaceLayout/main.c
+++ Pointer/001_VIrtualAddressSpaceLayout/main.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PRINT_ADDR(var) printf("=========> %s \t\t %p\n", #var, var);
+
 char szTitle[] = "Message Enter";
 
-void getMessage(char* msgBuf, size_t size) {
+void getMessage(char* buf, int size) {
+
+	PRINT_ADDR(getMessage);
+	PRINT_ADDR(&buf);
+	PRINT_ADDR(&size);
+
 	printf("%s : ", szTitle);
-	scanf_s("%s", msgBuf, size);
+	scanf_s("%s", buf, size);
 }
 
 int main() {
 	const int bufSize = 32;
-	char* msgBuf = malloc(bufSize);
+	char* msgBuf = NULL;
+
+	msgBuf = malloc(bufSize);
+	
+	PRINT_ADDR(szTitle);
+	PRINT_ADDR(main);
+	PRINT_ADDR(&bufSize);
+	PRINT_ADDR(&msgBuf);
+	PRINT_ADDR(msgBuf);
+
 	getMessage(msgBuf, bufSize);
 
 	printf("Message : %s\n", msgBuf);
+	
+	printf("Enter!!");
+	getch();
 
 	free(msgBuf);
 
