e3e54aeed8635360bfad45dba88bec789bc10ed8
hwia-edu@gmail.com <hwia-edu@gmail.com>
2020-05-19 11:37:28 +0900

    .

--- Pointer/001_VIrtualAddressSpaceLayout/main.c
+++ Pointer/001_VIrtualAddressSpaceLayout/main.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char szTitle[] = "Message Enter";
+
+void getMessage(char* msgBuf, size_t size) {
+	printf("%s : ", szTitle);
+	scanf_s("%s", msgBuf, size);
+}
+
 int main() {
+	const int bufSize = 32;
+	char* msgBuf = malloc(bufSize);
+	getMessage(msgBuf, bufSize);
+
+	printf("Message : %s\n", msgBuf);
+
+	free(msgBuf);
+
 	return 0;
 }
