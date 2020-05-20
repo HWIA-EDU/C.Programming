252167991821d2f70c2eb98743ab0d9aa519ce11
hwia-edu@gmail.com <hwia-edu@gmail.com>
2020-05-20 11:17:06 +0900

    Revert ".  이전상태로 돌리기"
    This reverts commit 18e9c37eec37621a6e928bc12d7ad1e94b7266f3.

--- Pointer/001_VIrtualAddressSpaceLayout/main.c
+++ Pointer/001_VIrtualAddressSpaceLayout/main.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include < conio.h >
-#define PRINT_ADDR(var) printf("=========> %s \t\t %p\n", #var, var);
+#include <conio.h>
+#include <string.h>
+#define PRINT_ADDR(var) printf("%s\t\t\t%p\n", #var, var);
 
-char szTitle[] = "Message Enter";
+int sum = 0;
 
-void getMessage(char* buf, int size) {
+void calc(int start, int end) {
+	sum = 0;
+	for (int i = start; i <= end; i++) {
+		sum += i;
+	}
 
-	PRINT_ADDR(getMessage);
-	PRINT_ADDR(&buf);
-	PRINT_ADDR(&size);
-
-	printf("%s : ", szTitle);
-	scanf_s("%s", buf, size);
+	char* msg = malloc(1024);
+	if (NULL != msg) {
+		memset(msg, 0, 1024);
+		snprintf(msg, 1024, "\n¥Ò(%d,%d) = %d\n\n", start, end, sum);
+		printf(msg);
+		free(msg);
+	}
 }
 
 int main() {
-	const int bufSize = 32;
-	char* msgBuf = NULL;
+	int start;
+	int end;
 
-	msgBuf = malloc(bufSize);
+	while (1) {
+		
+		printf("begin number : ");
+		scanf_s("%d", &start);
 
-	PRINT_ADDR(szTitle);
-	PRINT_ADDR(main);
-	PRINT_ADDR(&bufSize);
-	PRINT_ADDR(&msgBuf);
-	PRINT_ADDR(msgBuf);
-
-	getMessage(msgBuf, bufSize);
-
-	printf("Message : %s\n", msgBuf);
-
-	printf("Enter!!");
-
-	(void)_getch();
-
-	free(msgBuf);
+		printf("end number : ");
+		scanf_s("%d", &end);
+	
+		calc(start, end);
+	}
 
 	return 0;
 }
