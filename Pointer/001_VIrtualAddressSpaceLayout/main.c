584dd9bc4993e1fd679fb92eb496be7b768c7533
hwia-edu@gmail.com <hwia-edu@gmail.com>
2020-05-20 17:08:14 +0900

    .

--- Pointer/001_VIrtualAddressSpaceLayout/main.c
+++ Pointer/001_VIrtualAddressSpaceLayout/main.c
@@ -1,30 +1,21 @@
+/*
+사용자가 입력한 갯수만큼 
+렌 덤한 수를 생성하는 프로그램
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
-/* 
-사용자가 입력한 수 만큼 입의의 렌덤한 
-수를 생성하는 프로그램
-*/
+void createData(int cnt);
+void showData(int cnt);
+
 int* buffer = NULL;
 
-void createData(int cnt) {
-	srand(time(NULL));
-	for (int i = 0; i < cnt;i++) {
-		buffer[i] = rand();
-	}
-}
-
-void showData(int cnt) {
-	for (int i = 0; i < cnt; i++) {
-		printf("%d\n", buffer[i]);
-	}
-}
-
-int main() {
+int main() 
+{
 	int count = 0;
 
-	printf("생성할 수를 입력하세요 : ");
+	printf("Enter Number : ");
 	scanf_s("%d", &count);
 
 	buffer = malloc(sizeof(int) * count);
@@ -35,4 +26,19 @@ int main() {
 	}
 	
 	return 0;
+}
+
+void createData(int cnt) 
+{
+	srand(time(NULL));
+	for (int i = 0; i < cnt; i++) {
+		buffer[i] = rand();
+	}
+}
+
+void showData(int cnt) 
+{
+	for (int i = 0; i < cnt; i++) {
+		printf("%d\n", buffer[i]);
+	}
 }
