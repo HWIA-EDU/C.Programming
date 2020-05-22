20c72e51f9d04f81431837c9ad4849ffc9d2036c
hwia-edu@gmail.com <hwia-edu@gmail.com>
2020-05-22 18:40:47 +0900

    .

--- Pointer/002_DataAndDataType/main.c
+++ Pointer/002_DataAndDataType/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main() {
-	printf("%c\n",'A');
+
+	printf("%c\n", 'A');
+	printf("%s\n", "ABC");
+
+	//문자의 정수 표현
 	printf("%c\n",65);
+
+	//문자열의 정수 표현
+	char arr[] = { 65, 66, 66,0 };
+	printf("%s\n", &arr);
+
+	int n = 4407873;
+	printf("%s\n",&n);
+	
 	return 0;
 }
