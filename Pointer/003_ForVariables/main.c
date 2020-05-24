1e87ed87eb856a7ff4c9abfdc7e7083a43189c96
hwia-edu@gmail.com <hwia-edu@gmail.com>
2020-05-24 16:10:42 +0900

    .

--- Pointer/003_ForVariables/main.c
+++ Pointer/003_ForVariables/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+
+struct Point {
+	int x;
+	int y;
+};
+
 int main() {
+	char c = 'A';
+	printf("%c\n", c);
+
+	int n = 100;
+	printf("%d\n", n);
+
+	struct Point pt = { 10, 20 };
+	printf("%d, %d\n", pt.x, pt.y);
+
 	int arr[2][3] = { {1,2,3},{4,5,6} };
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 3; j++) {
