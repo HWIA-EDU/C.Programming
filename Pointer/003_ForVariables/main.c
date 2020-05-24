b6f16cf1f9d5db8c04dba5020a2aaaca7f556c92
HWIA <hwia.edu@gmail.com>
2020-05-24 13:13:48 +0900

    .

--- Pointer/003_ForVariables/main.c
+++ Pointer/003_ForVariables/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+
+struct Point {
+	int x;
+	int y;
+};
+
+typedef int ARRAY_5INT[5];
 int main() {
-	printf("Hello world");
+
+	printf("%d", sizeof(int[5]));
+
+	ARRAY_5INT arr;
+	
 	return 0;
 }
