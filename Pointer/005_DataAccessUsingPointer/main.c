fe9ab86e1cd843ec8e4e71ee807cf40d619d8bba
hwia-edu@gmail.com <hwia-edu@gmail.com>
2020-05-30 23:41:38 +0900

    .

--- Pointer/005_DataAccessUsingPointer/main.c
+++ Pointer/005_DataAccessUsingPointer/main.c
@@ -1,32 +1,55 @@
-#include <windows.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-#define TRACE(fmt,...) {\
-	char _c_buf[1024] = { 0, };\
-	_snprintf_s(_c_buf,sizeof(_c_buf),sizeof(_c_buf),"% 3d > " fmt, __LINE__, __VA_ARGS__);\
-	OutputDebugString(_c_buf);\
-	printf(_c_buf);\
+#include <windows.h>
+void _trace(const char* fmt, ...) {
+	char _cbuf[102] = { 0, };
+	va_list args;
+
+	va_start(args, fmt);
+	vsnprintf_s(_cbuf, sizeof(_cbuf), sizeof(_cbuf), fmt, args);
+	va_end(args);
+
+	OutputDebugString(_cbuf);
+
+	printf("%s", _cbuf);
 }
 
+#define TRACE(fmt,...) _trace("% 3d > " fmt, __LINE__,__VA_ARGS__)
+
 int main() {
-	/* 일반 초기화 */
-	int num = 100;
-	TRACE("&num : %p\n", &num);
-	TRACE("num : %d\n", num);
+	/* 일반 변수 초기화 */
+	int num = 10;
 
-	/* 포인터변수 초기화*/
+	/* 포인터 변수 초기화 */
 	int* ptr = NULL;
-	TRACE("&ptr : %p\n", &ptr);
+
+	//변수의 메모리 주소 확인
+	TRACE("&num : %p \n", &num);
+	TRACE("&ptr : %p \n", &ptr);
+
+	//변수에 저장된 값 확인	
+	TRACE("num : %d \n", num);
+	TRACE("ptr : %p \n", ptr);
+
+	//num  변수의 주소를 ptr변수에 저장
+	ptr = &num; 
 	TRACE("ptr : %p\n", ptr);
 
-	/* num 의 메모리 주소를 포인터 ptr에 저장 */
-	ptr = &num;
-	TRACE("ptr : %p\n", ptr);
-
-	/* 간접참조 연산자를 통해서 값 읽기 */
+	//간접참조 연산자를 통해서 값 읽기 
 	TRACE("*ptr : %d\n", *ptr);
 
-	/* 간접참조 연산자를 통해서 값 쓰기 */
+	//간접참조 연산자를 통해서 값 쓰기 
 	*ptr = 500;
 	TRACE("num : %d\n", num);
+
+	//임의 기준 주소 해제 상태에서 확인 가능 
+	//프로젝트 속성 > 링크 > 고급 > 임의 기준 주소 > 아니요(/DYNAMICBASE:NO) 
+	//보안에 취약함으로 반드시 활성화 상태로 유지 !!
+	*((int*)0x0019FED8) = 1000;
+	TRACE("num : %d\n", num);
+	TRACE("*ptr : %d\n", *((int*)0x0019FED8));
+
+	getch();
+
 }
