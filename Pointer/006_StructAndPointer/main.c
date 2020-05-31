0fd3c032d5df519787a4b160f6eac899e0c8af30
hwia-edu@gmail.com <hwia-edu@gmail.com>
2020-05-31 16:33:55 +0900

    .

--- Pointer/006_StructAndPointer/main.c
+++ Pointer/006_StructAndPointer/main.c
@@ -26,38 +26,50 @@ struct Point {
 int main() {
 	struct Point pos = { 1,2 };
 	
+	//직접 접근
 	TRACE("pos.x = %d\n", pos.x);
 	TRACE("pos.y = %d\n", pos.y);
 
+	//메모리 구조 확인
 	TRACE("&pos = %p\n", &pos);
 	TRACE("&pos.x = %p\n", &pos.x);
 	TRACE("&pos.y = %p\n", &pos.y);
 	
+	//포인터 변수 선언
 	struct Point* ptr = NULL;
 	TRACE("&ptr = %p\n", &ptr);
 	
+	//포인터 변수에 pos 변수의 주소 저장
 	ptr = &pos;
 	TRACE("ptr = %p\n", ptr);
-
-	//포인터를 이용한 멤버 읽기
-	TRACE("ptr->x = %d\n", ptr->x);
-	TRACE("ptr->y = %d\n", ptr->y);
 
 	//간접참조를 이용한 멤버 일기
 	TRACE("(*ptr).x = %d\n", (*ptr).x);
 	TRACE("(*ptr).y = %d\n", (*ptr).y);
 
-	//포인터를 이용한 멤버 쓰기
-	ptr->x = 10;
-	ptr->y = 20;
+	//Arrow 연산자를 이용한 멤버 읽기
+	TRACE("ptr->x = %d\n", ptr->x);
+	TRACE("ptr->y = %d\n", ptr->y);
+
+	//간접참조를 이용한 멤버 쓰기
+	(*ptr).x = 10;
+	(*ptr).y = 20;
 	TRACE("pos.x = %d\n", pos.x);
 	TRACE("pos.y = %d\n", pos.y);
 
-	//간접참조를 이용한 멤버 쓰기
-	(*ptr).x = 100;
-	(*ptr).y = 200;
+	//Arrow 연산자를 이용한 멤버 쓰기
+	ptr->x = 100;
+	ptr->y = 200;
 	TRACE("pos.x = %d\n", pos.x);
 	TRACE("pos.y = %d\n", pos.y);
+
+	//간접참조 ,Arrow 연산자를 이용한 멤버 주소 접근
+	TRACE("&pos.x = %p\n", &(*ptr).x);
+	TRACE("&pos.y = %p\n", &ptr->y);
+
+	TRACE("&pos.x = %p\n", &((*ptr).x));
+	TRACE("&pos.y = %p\n", &(ptr->y));
+
 
 	return 0;
 }
